83265f1f96106d7a286ddfdeef3cad68ec133871
Ashleigh Clark <ashleigh.clark@proton.me>
2024-01-21 14:16:25 +0200

    Added pseudo-chats to window.

---
+++ src/client/ViewModels/ContactDisplayViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace com.ashleigh.angora.client.ViewModels
+{
+	public class ContactDisplayViewModel
+	{
+		public string FullName { get; set; } = string.Empty;
+
+		public string LastMessage { get; set; } = string.Empty;
+
+		public bool LastMessageDelivered { get; set; } = false;
+
+		public DateTime? LastMessageDeliveredTime { get; set; }
+
+		public bool LastMessageRead { get; set; } = false;
+
+		public DateTime? LastMessageReadTime { get; set; }
+
+	}
+}
